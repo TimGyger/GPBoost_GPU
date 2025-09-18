2a7258e9dc31ada4b23e8218a8501ff444a2254a
TimGyger <tim.gyger@bluewin.ch>
2025-09-18 15:19:13 +0200

    Vecchia Components

--- src/GPBoost/cuda_kernel.cu
+++ src/GPBoost/cuda_kernel.cu
@@ -421,7 +421,7 @@ namespace GPBoost {
     cudaError_t err = call;                                                  \
     if (err != cudaSuccess) {                                                \
         fprintf(stderr, "CUDA error at %s:%d: %s\n",                         \
-                __FILE__, __LINE__, cudaGetErrorString(err));                \
+                __FILE__, __LINE__, cudaGetErrorString(err));fflush(stdout); \
         exit(EXIT_FAILURE);                                                  \
     }                                                                        \
 }
