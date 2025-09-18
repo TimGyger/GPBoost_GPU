96add09ef2eef27a82aed6e50cb6947c42281f8a
TimGyger <tim.gyger@bluewin.ch>
2025-09-18 16:47:16 +0200

    Vecchia Components

--- src/GPBoost/Vecchia_utils.cpp
+++ src/GPBoost/Vecchia_utils.cpp
@@ -1611,7 +1611,7 @@ namespace GPBoost {
 		}
 		end = std::chrono::steady_clock::now();//only for debugging
 		el_time = (double)(std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count()) / 1000000.;//only for debugging
-		Log::REInfo("Vecchia BDB time until = %g and val %g and %g ", el_time, D_inv_cluster_i.coeffRef(10, 10), D_inv_cluster_i.coeffRef(0, 0));
+		Log::REInfo("Vecchia BDB time until = %g and val %g and %g and %g and %g ", el_time, D_inv_cluster_i.coeffRef(10, 10), D_inv_cluster_i.coeffRef(0, 0), D_inv_cluster_i.diagonal().minCoeff(), D_inv_cluster_i.diagonal().maxCoeff());
 	}//end CalcCovFactorGradientVecchia
 
 	void CalcPredVecchiaObservedFirstOrder(bool CondObsOnly,
--- src/GPBoost/cuda_kernel.cu
+++ src/GPBoost/cuda_kernel.cu
@@ -199,7 +199,9 @@ namespace GPBoost {
     ) {
         int i = blockIdx.x * blockDim.x + threadIdx.x;
         if (i >= n) return;
-
+        if (i == 10) {
+            printf("Thread\n");
+        }
         int start = nn_ptr[i];
         int end = nn_ptr[i + 1];
         int total_nnz = nn_ptr[n]; // length of flattened neighbor list 
