a4958641b83bf320c97d53f491dec93289a68c54
TimGyger <tim.gyger@bluewin.ch>
2025-10-01 17:01:56 +0200

    Vecchia Components

--- src/GPBoost/cuda_kernel.cu
+++ src/GPBoost/cuda_kernel.cu
@@ -126,9 +126,8 @@ namespace GPBoost {
         double corr_diag_i = corr_diag[i];
         // each thread checks candidates j < i
         for (int j = tid; j < i; j += blockDim.x) {
-            const double* __restrict__ col_j = chol_ip_cross_cov + j * num_ip;
+            
             const double* __restrict__ coord_j_ptr = coords + j * d;
-
             double sum = 0.0;
             for (int dd = start_dim; dd < d; dd++) {
                 double diff = coord_j_ptr[dd] - coord_i_ptr[dd];
@@ -138,15 +137,15 @@ namespace GPBoost {
             double range_dist = 0.;
             double var = pars[0];
             double dot = 0.0;
-#pragma unroll 8
-            for (int dd = 0; dd < num_ip; dd++) {
-                dot = fma(col_j[dd], col_i[dd], dot);
-            }
             if (dist_funct == 1) {
+                const double* __restrict__ col_j = chol_ip_cross_cov + j * num_ip;
+                for (int dd = 0; dd < num_ip; dd++) {
+                    dot = fma(col_j[dd], col_i[dd], dot);
+                }
                 range_dist = range / inv_r;
             }
             else if (dist_funct == 2) {
-                double dt = fabs(col_j[0] - col_i[0]);
+                double dt = fabs(coord_i_ptr[0] - coord_j_ptr[0]);
                 double d_aux_time_log =  (dt < EPSILON_NUMBERS) ? 0. : log(pars[1] * exp(log(dt) * 2 * pars[3]) + 1.);
                 range_dist = pars[2] / (exp(d_aux_time_log * pars[5] * 0.5) * inv_r);
                 var = pars[0] / (exp(d_aux_time_log * (pars[6] + pars[5] * (d - 1) * 0.5)));
